1deca655694ca5c4f22368f2d6e3e01cc765fd60
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-03-24 18:31:25 +0530

    Thirteenth question,second array list

---
+++ Milestone-7/Q2-13.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int size;
+    cout<<"Enter the size of the array \n";
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        int number;
+        cin>>number;
+        arr[i]=number;
+    }
+    unordered_map<int,int> map;
+    for(int ele:arr){
+        map[ele]++;
+    }cout<<"\n";
+    int countDup=0;
+
+    for(int ele:arr){
+        if(map[ele]>1){
+            countDup++;
+        }
+    }
+    cout<<"Number of duplicate elements = "<<countDup/2;
+}
