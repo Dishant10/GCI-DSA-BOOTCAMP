fe686f4cade767e7693907e6398c90a8cb5b48ba
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-03-24 19:04:48 +0530

    Fifteenth question,second array list

---
+++ Milestone-7/Q2-15.cpp
@@ -0,0 +1,39 @@
+ 
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int size;
+    cout<<"Enter the size of the array \n";
+    cin>>size;
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+        int number;
+        cin>>number;
+        arr[i]=number;
+    }
+
+    int size2;
+    cout<<"Enter the size of the second array";
+    cin>>size2;
+    int arr2[size2];
+    for(int i=0;i<size2;i++){
+        int number;
+        cin>>number;
+        arr2[i]=number;
+    }
+
+    int mergeArray[size+size2];
+    for(int i=0;i<size;++i){
+        mergeArray[i]=arr[i];
+    }
+    for(int i=0,j=size;i<size2;++i,++j){
+        mergeArray[j]=arr2[i];
+    }
+
+    cout<<"Array after merging \n";
+    for(int ele:mergeArray){
+        cout<<ele;
+    }
+
+}
